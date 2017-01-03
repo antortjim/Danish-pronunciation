b6af95736a8b4fa9bf8e1a0eea69992ed82eae1c
antortjim <ntoniohu@gmail.com>
2017-01-03 23:31:09 +0100

    Added support for Forvo using private key

--- download_audio.R
+++ download_audio.R
@@ -1,10 +1,11 @@
 # Main functions
 
-lapply(list("rvest", "httr", "dplyr", "stringr"), function(x) suppressMessages(library(x, character.only=TRUE)))
+lapply(list("rvest", "httr", "dplyr", "stringr", "xml2"), function(x) suppressMessages(library(x, character.only=TRUE)))
 
+not.found <- "No entry/audio found at "
+found <- "Success at "
 
-
-wiktionary.download <- function(query)
+wiktionary.download <- function(query, base.filename)
 {
   # Build url for query
   wiktionary.url <- paste("https://en.wiktionary.org/wiki/", query, "#Danish", sep = "")  
@@ -19,40 +20,47 @@ wiktionary.download <- function(query)
    html_attr("src")
    
    
-   lang <- tolower(str_match(pattern= ".*\\/(\\w{2})-\\w*.ogg", audio)[,2])
    # Check if Danish audio file was found
-   if(!is.na(audio) && lang == "da") {
-     
+   if(!is.na(audio)) {
+     lang <- tolower(str_match(pattern= ".*\\/(\\w{2})-\\w*.ogg", audio)[,2])
+     if(lang != "da")
+     {
+       err.message <- paste("Audio file found for", query, "at Wiktionary but in language", lang, sep = " ")
+       message(err.message)
+       invisible(FALSE)
+     } else {
      # If audio file is found, refine the url and download it
      # The url refinement adds https: in case it does not have it (requiered for downlad.file later)
      correct <- unlist(strsplit(audio, split = ""))[1] == "h"
-     print(correct)
+     #print(correct)
      if(!correct) {
        audio <- paste("https:", audio, sep = "")
      }
 
-     message("Found audio file at Wiktionary !")
-     filename <- paste(query, "ogg", sep = ".")
-     print(audio)
+     message(found, "Wiktionary !")
+     filename <- paste(base.filename, "ogg", sep = ".")
+     
+     #print(audio)
      download.file(audio, destfile = filename, quiet = TRUE)
      invisible(TRUE)
      
      
-   } else  {
+   }} else  {
        # Otherwise (entry exists but without Danish audio file), send error message and exit
-       err.message <- paste("No pronunciation file found for", query, "at Wiktionary", sep = " ")
+       err.message <- paste(not.found, "Wiktionary", sep = " ")
        message(err.message)
        invisible(FALSE)
   }} else  {
       # Return error if url does not exist (entry does not exist)
-      err.message <- paste("No entry for", query, "at Wiktionary", sep = " ")
+      err.message <- paste(not.found, "Wiktionary", sep = " ")
       message(err.message)
       invisible(FALSE)
    }
 }
 
-ddo.download <- function(query)
+ddo.download <- function(query, base.filename)
 {
+ message("Searching Den Danske Ordbog")
  # Build url for query
  ddo.base <- "http://ordnet.dk/ddo/ordbog?query="
  ddo.url <- paste(ddo.base, query, sep = "")
@@ -69,15 +77,52 @@ ddo.download <- function(query)
  # Check if audio file was found
  if(length(audio) != 0) {
    # If audio file is found, download it
-   message(paste("Found audio file at DDO !", sep = ""))
-   filename <- paste(query, "mp3", sep = ".")
+   message(paste(found, "DDO !", sep = ""))
+   filename <- paste(base.filename, "mp3", sep = ".")
    download.file(audio, destfile = filename, quiet = TRUE)
+   message("Saving to file ", base.filename, ".mp3", sep = "")
    invisible(TRUE)
    
  } else {
    # Otherwise, send error message and exit
-   err.message <- paste("No pronunciation file found for", query, "at DDO", sep = " ")
+   err.message <- paste(not.found, "DDO", sep = " ")
    message(err.message)
    invisible(FALSE)
  }
 }
+
+
+
+fileName <- 'forvo_key'
+forvo_key <- readChar(fileName, file.info(fileName)$size)
+
+forvo.download <- function(query, base.filename)
+{
+  message("Searching Forvo")
+  # Build url for query using FORVO api
+  forvo.url <- paste("https://apifree.forvo.com/key", forvo_key, "format/xml/action/word-pronunciations/word",
+                     query, "language/da", sep = "/")
+  
+  # Read url
+  ord.entry <- read_xml(forvo.url)
+  
+  if(!length(ord.entry %>% xml_children) == 0)
+  {
+    # Parse url. By default choose first option
+    audio.url <- ((ord.entry %>% xml_children)[1] %>% xml_contents)[12] %>% xml_text
+    audio.file <- paste(base.filename, "ogg", sep = ".")
+    # Download
+    download.file(url = audio.url, destfile = audio.file, quiet = TRUE)
+    # Return exit status
+    message(found, "Forvo")
+    message("Saving to file ", base.filename, ".ogg", sep = "")
+    
+    invisible(TRUE)
+  } else {
+    # Not found in forvo
+    message(not.found, "Forvo!")
+    invisible(FALSE)
+  }
+
+}
+
--- udtale.R
+++ udtale.R
@@ -4,7 +4,19 @@
 source("download_audio.R")
 
 # Input query danish word from standard input
-query <- commandArgs(trailingOnly = TRUE)[1]
+arguments <- commandArgs(trailingOnly = TRUE)
+
+#print(length(arguments))
+# test if there is at least one argument: if not, return an error
+if (length(arguments) == 0) {
+  stop("At least one argument must be supplied", call. = FALSE)
+} else if (length(arguments) == 1) {
+  # default output file
+  query <- arguments[1]
+} else if (length(arguments) == 2) {
+  query <- paste(arguments[1:2], collapse = " ")
+}
+
 
 # Read parameters table in input_parameters file
 parameters <- read.table(file = "input_parameters", stringsAsFactors = F)
@@ -15,5 +27,16 @@ destination.folder <- filter(parameters, V1 == "destination.folder")[,2]
 # Set working directory to destination folder
 setwd(destination.folder)
 
-# Call ddo first. If unsuccesful, try wiktionary
-do.call("ddo.download", list(query)) || do.call("wiktionary.download", list(query)) 
+#Builid base.filename
+base.filename <- gsub(" ", "_", query)
+
+# Call forvo first. If unsuccesful try ddo. If unsuccesful, try wiktionary
+#`%||%` <- function(a, b) if (!is.null(a)) a else b
+do.call("ddo.download", list(query, base.filename)) || do.call("forvo.download", list(query, base.filename))
+#|| do.call("wiktionary.download", list(query, base.filename)) 
+
+# Output destination file message
+
+# Play the downloaded file
+command <- paste("play ", base.filename, ".*", sep = "")
+system(command)
